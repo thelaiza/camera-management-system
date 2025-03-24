84246272d295487a8a382b949bb8f3f099f1f74d
Laíza <silvacarolinalaiza@gmail.com>
2025-03-24 19:40:19 -0300

    configurando o servidor, pagina home criada

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,15 @@
 
 from django.urls import path
 from . import views
+from django.urls import path
+from .views import home
 
 urlpatterns = [
     path('cameras/', views.listar_cameras, name='listar_cameras'),
     path('usuarios/', views.listar_usuarios, name='listar_usuarios'),
+]
+
+
+urlpatterns = [
+    path("", home, name="home"),  
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,18 @@
 # Lógica de requisições HTTP (tipo os controllers do Express)
 
 from django.http import JsonResponse
+from django.http import HttpResponse
 
 def listar_cameras(request):
     return JsonResponse({"message": "Listando câmeras"})
 
 def listar_usuarios(request):
     return JsonResponse({"message": "Listando usuários"})
+
+def home(request):
+    return HttpResponse("Bem-vindo ao CamIA Manager!")
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "home.html")
