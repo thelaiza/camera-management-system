f3763445d60608f6c6e42a3eb7c64eba9c6e61ed
Laíza <silvacarolinalaiza@gmail.com>
2025-05-13 11:42:09 -0300

    removendo problemas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,15 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import CamerasPage from "./pages/CamerasPage";
 import UsuariosPage from "./pages/UsuariosPage";
 import LogsPage from "./pages/LogsPage";
 
-
 function App() {
-  const [data, setData] = useState(null);
-
-  // Requisição ao backend Django
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/") 
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error("Erro ao buscar dados:", error));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-        <Routes>
+    <Router>
+      <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/cameras" element={<CamerasPage />} />
@@ -29,10 +17,7 @@ function App() {
         <Route path="/logs" element={<LogsPage />} />
       </Routes>
     </Router>
-      </header>
-    </div>
   );
 }
-
 
 export default App;
